745acc8e565b749c61a399e2b2ba2b1b8aa488f5
Willem A Duiker <duikhead2@gmail.com>
2020-08-22 17:02:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,9 @@
 def reverse_each_word(str)
   new_string = ""
   new_array = str.split(/ /)
-  new_array.collect{ |word| if word != new_array[0] { word = word + " " } word.reverse }.join
+  new_array.collect{ |word|
+  if word != new_array[0]
+    word = word + " "
+    end
+    word.reverse }.join
 end
